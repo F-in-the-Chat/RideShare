224c36a35d666865e8911e9ab1e03e32cead52b6
Jared P <jj21111@yahoo.com>
2021-11-04 13:33:46 -0400

    joinRide framework 2

--- server/joinRide.js
+++ server/joinRide.js
@@ -2,10 +2,16 @@ const express = require("express");
 const app = express();
 const port = 5001;
 
+app.use(express.json())
+
 app.listen(port, "0.0.0.0", () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
 
 app.get("/join", (req, res) => {
-  res.send({ Hello: "World" });
+    let user = req.body["user"]
+    let ride = req.body["ride"]
+    //Write user and ride to DB
+    //Send event for ride joined
+    res.send({ Status: "Success" });
 });
