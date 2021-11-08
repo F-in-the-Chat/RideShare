6664c602bda8806b7d05accc6f62d6514f322054
Roshan Shetty <roshanshetty2000@gmail.com>
2021-11-07 20:34:12 -0500

    removed bodyParser

--- server/events.js
+++ server/events.js
@@ -1,32 +1,33 @@
 const express = require("express");
-const bodyParser = require('body-parser')
 const app = express();
 const axios = require("axios");
 const port = 5002;
-app.use(bodyParser.json())
+app.use(express.json());
 
-let event_subs = {}
+let event_subs = {};
 
 app.listen(port, "0.0.0.0", () => {
-    console.log(`Event Bus listening at http://localhost:${port}`);
+  console.log(`Event Bus listening at http://localhost:${port}`);
 });
 //Endpoint that recieves events and relays them to relevent microservices
 app.post("/events", (req, res) => {
-    const event = req.body;
-    event_subs[event.name].forEach(sub => {
-        axios.post(sub, event).catch((err) => {
-            console.log(err.message);
-        });
+  const event = req.body;
+  event_subs[event.name].forEach((sub) => {
+    axios.post(sub, event).catch((err) => {
+      console.log(err.message);
     });
-    res.send({ status: "OK" });
+  });
+  res.send({ status: "OK" });
 });
 //Endpoint for microservices to subscribe to certain events
-app.post("/subscribe", (req,res)=>{
-    const address = req.body.address
-    let events = req.body.events
-    events.forEach(event=>{
-        event_subs[event] === undefined?event_subs[event] = [address]:event_subs[event].push(address)
-    })
-    console.log(event_subs)
-    res.send({ status: "OK" });
-})
+app.post("/subscribe", (req, res) => {
+  const address = req.body.address;
+  let events = req.body.events;
+  events.forEach((event) => {
+    event_subs[event] === undefined
+      ? (event_subs[event] = [address])
+      : event_subs[event].push(address);
+  });
+  console.log(event_subs);
+  res.send({ status: "OK" });
+});
