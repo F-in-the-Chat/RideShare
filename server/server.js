d2234e99ae69131305809e76d915b78759fd615c
Roshan Shetty <roshanshetty2000@gmail.com>
2021-11-07 20:47:57 -0500

    files renamed and restructured

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+// All the routes would be added here example:
+// const createRideRoute = require('../routes/createRide/createRide')
+
 app.listen(port, "0.0.0.0", () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
