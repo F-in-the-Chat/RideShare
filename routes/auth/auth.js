ee000e8b97a84a968cd28284c15cda4f3a224828
Eric Li <ericli@umass.edu>
2021-11-09 19:52:05 -0500

    big leap forward

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -1,21 +1,24 @@
 const express = require("express");
 const app = express();
 const { MongoClient } = require("mongodb");
-// var url = ;
-//https://cloud.mongodb.com/v2/618ad7b544c69666db2fb5d5#clusters/detail/testLogin
-// all might be post since token needs to be added and removed for login and logout respectively
-// add jwt token to this, look at middleware
+var url = "mongodb+srv://testlogin.taf1q.mongodb.net/myFirstDatabase?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority";
+const client = new MongoClient(url);
+await client.connect();
+
 const jwt = require("jsonwebtoken");
+TOKEN_SECRET = F1n7hEcH47; //FInTheChat
+const dotenv = require('dotenv');
 
+dotenv.config();
+process.env.TOKEN_SECRET;
 
-app.get('.../login/', (req,res) => { // get request. we are only check with database, no changes made? maybe token
-    //let username = req.body["user"];
-    //let secret = req.body["password"];
-    let username = "gg@gmail.com";
-    let secret = "getRekted";
-    
+app.post('.../login/', (req,res) => { // get request. we are only check with database, no changes made? maybe token
+    let username = req.body["user"];
+    let secret = req.body["password"];
+    //let username = "gg@gmail.com";
+    //let secret = "getRekted";
     try{
-        //db.inventory.find({email:username})
+        let info = db.inventory.find({email:username}) //id, email, password
         //check username if username exists in database
         if (test.email!=username){
             throw new Error("Username doesn't exist")
@@ -23,14 +26,18 @@ app.get('.../login/', (req,res) => { // get request. we are only check with data
         if (test.password!=secret){
             throw new Error("Password doesn't match")
         }
+    } catch (err){
+        res.status().send(err);
+    }
+    try{
         // create token
-        // db.collection('inventory').updateOne( { token: "paper" } ).then(function(result) {})
-        // process result
-
+        let coin = jwt.sign(username, process.env.TOKEN_SECRET, { expiresIn: '1800s' })
+        // db.collection('inventory').updateOne( { token: coin } ).then(function(result) {})
         res.send({status: "Login successful"});
     } catch (err){
         res.status().send(err);
     }
+
 })
 
 app.post('.../signup/', (req,res) => { //post request bc we are adding to database
@@ -47,7 +54,7 @@ app.post('.../signup/', (req,res) => { //post request bc we are adding to databa
     }
 })
 
-app.get('.../logout/', (req,res) => { // get request. we are only check with database, no changes made? maybe token
+app.post('.../logout/', (req,res) => { 
     // let token = req.body.token
     // throw new Error("Logout unsuccessful")
     try{
---
+++ routes/auth/extra.js
@@ -0,0 +1,10 @@
+//don't delete yet, just need extra space- Eric
+
+//express jwt source, https://www.digitalocean.com/community/tutorials/nodejs-jwt-expressjs
+
+//node js, https://developer.okta.com/blog/2018/11/13/create-and-verify-jwts-with-node
+//const jwt = require('njwt')
+//const claims = { iss: 'fun-with-jwts', sub: 'AzureDiamond' }
+//const token = jwt.create(claims, 'top-secret-phrase')
+//token.setExpiration(new Date().getTime() + 60*1000)
+//res.send(token.compact())
