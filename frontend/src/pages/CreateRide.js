e60beb48e9ea9913aa1c3a101f0d71e8f0c147cb
Roshan Shetty <roshanshetty2000@gmail.com>
2021-11-21 02:10:02 -0500

    navigates to the rides list page once the button is clicked to ensure that the listing has been made

--- frontend/src/pages/CreateRide.js
+++ frontend/src/pages/CreateRide.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import CreateRideForm from "../component/rides/CreateRideForm";
 
 function CreateRide() {
+  const history = useHistory();
   function createRideHandler(ride) {
     fetch("/createRide", {
       method: "POST",
@@ -9,6 +11,9 @@ function CreateRide() {
       headers: {
         "Content-Type": "application/json",
       },
+    }).then(() => {
+      // It should to /rides but as of now the default page is / so it'll go there
+      history.replace("/");
     });
   }
   return (
