068df4c7d51afa9e0746f560968184798a0d2184
Roshan Shetty <roshanshetty2000@gmail.com>
2021-11-21 01:52:12 -0500

    Implemented ride code to add data to the database

--- frontend/src/pages/CreateRide.js
+++ frontend/src/pages/CreateRide.js
@@ -2,10 +2,19 @@ import React from "react";
 import CreateRideForm from "../component/rides/CreateRideForm";
 
 function CreateRide() {
+  function createRideHandler(ride) {
+    fetch("/createRide", {
+      method: "POST",
+      body: JSON.stringify(ride),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
   return (
     <section>
       <h1>Create Ride </h1>
-      <CreateRideForm />
+      <CreateRideForm onCreateRide={createRideHandler} />
     </section>
   );
 }
