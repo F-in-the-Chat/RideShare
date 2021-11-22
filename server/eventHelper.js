23ea6ca4cf6b097bbd44947ff5070a502b4870a0
Eric Li <ericli@umass.edu>
2021-11-22 16:04:42 -0500

    notes

--- server/eventHelper.js
+++ server/eventHelper.js
@@ -2,8 +2,8 @@ const axios = require("axios");
 
 //Function to send events to the event bus
 //Import with const eventHelper = require(path/to/eventHelper.js)
-function sendEvent(event_name,event_data){
-    axios.post("http://localhost:5002/events", {name: event_name,data:event_data}).catch((err) => {
+function sendEvent(event_name,event_data){ // name of event and data for event sent to eventbus.js
+    axios.post("http://localhost:5002/events", {name: event_name,data:event_data}).catch((err) => { // look at eventbus.js
         console.log(err.message);
     });
   }
