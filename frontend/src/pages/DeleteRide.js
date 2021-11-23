5f70529a26ddde0a581ce3768c6a6cc85b32708e
Rishab Maheshwari <40054912+rishabmahesh@users.noreply.github.com>
2021-11-23 01:50:30 -0500

    DeleteRide update

--- frontend/src/pages/DeleteRide.js
+++ frontend/src/pages/DeleteRide.js
@@ -1,40 +1,59 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import SecondRideList from "../component/rides/SecondRideList";
+const axios = require("axios");
 
-const dummy = [
-    {
-      id: "sdfnmsdklfnmsdlf",
-      title: "Ride 1",
-      date: "some date",
-      pickup: "dfsd",
-      dropoff: "sdfsdfsd",
-      car_capacity: 4,
-      price: 35,
-      preferences: "sdfsdnfksdjf",
-      creator: "sdfsd",
-    },
-    {
-      id: "sdfgsdg",
-      title: "Ride 2",
-      date: "some date",
-      pickup: "sfgdfg",
-      dropoff: "sdfgsdrfh",
-      car_capacity: 4,
-      price: 35,
-      preferences: "sdfgsdtgnfksdjf",
-      creator: "Sdfsdf",
-    },
-];
-  
-
+// HAVE TO EDIT THIS SUCH THAT WE GET THE RIDES MADE BY THE PARTICULAR USER - JUST DISPLAYING ALL RIDES FOR NOW
+// MIGHT HAVE TO LINK RIDES WITH USERS (ESPECIALLY DRIVERS) 
 function DeleteRide() {
-    //have to display rides created by authenticated user - displaying dummy rides for now
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedRides, setLoadedRides] = useState([]);
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("http://localhost:5005/getRide")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        const rides = [];
+        for (const key in data) {
+          const ride = {
+            id: key,
+            ...data[key],
+          };
+
+          rides.push(ride);
+        }
+        setIsLoading(false);
+        setLoadedRides(rides);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
+  if (isLoading) {
     return (
-        <section>
-            <h1>Available Rides</h1>
-            <SecondRideList rides={dummy} />
-        </section>
+      <section>
+        <p>Loading...</p>
+      </section>
     );
+  }
+
+  function deleteRideHandler(ride) {
+    console.log("Ride in deleteRideHandler: " + ride.rideTitle)
+    axios
+      .post("http://localhost:5006/deleteRide", { ride})
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }
+
+  return (
+      <section>
+          <h1>Rides Created by You</h1>
+          <SecondRideList rides={loadedRides} deleteRide={deleteRideHandler}/>
+      </section>
+  );
 }
 
 export default DeleteRide;
