b95de99de5a9466aad925fde52422bdd0b54eaed
Rishab Maheshwari <40054912+rishabmahesh@users.noreply.github.com>
2021-11-23 05:27:41 -0500

    Update JoinedRides.js
    Frontend list, getting user and ride needs to be implemented

--- frontend/src/pages/JoinedRides.js
+++ frontend/src/pages/JoinedRides.js
@@ -1,6 +1,25 @@
 import React from "react";
 
+// TODO: Frontend list, getting user and ride to be implemented
+
 function JoinedRides() {
+
+  function joinRideHandler(ride) {
+    axios
+      .post("http://localhost:5001/joinRide", { ride })
+      .catch((err) => {
+        console.log(err.message);
+    });
+  }
+
+  function cancelJoinRideHandler(ride) {
+    axios
+      .post("http://localhost:5007/cancelJoin", { ride })
+      .catch((err) => {
+        console.log(err.message);
+    });
+  }
+
   return <div>Joined Rides Page</div>;
 }
 
