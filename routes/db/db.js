e2773adefe151be73afcc44768a8576cb6d12a95
Eric Li <ericli@umass.edu>
2021-11-22 19:08:11 -0500

    added

--- routes/db/db.js
+++ routes/db/db.js
@@ -13,6 +13,9 @@ const User = require("../../models/user.model")
 const eventHandlers = {
     "test":testEventHandler,
     "createRide":createRide,
+    "Search":search,
+    "createUser":createUser,
+    "deleteToken":deleteToken,
 }
 
 app.listen(port, "0.0.0.0", () => {
