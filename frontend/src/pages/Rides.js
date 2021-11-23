0ba51ccd1870d23f442f760f33e8bf7695976a6f
Roshan Shetty <roshanshetty2000@gmail.com>
2021-11-22 19:18:00 -0500

    Minor styling changes

--- frontend/src/pages/Rides.js
+++ frontend/src/pages/Rides.js
@@ -1,40 +1,5 @@
 import { useState, useEffect } from "react";
 import RideList from "../component/rides/RideList";
-const axios = require("axios");
-
-// const dummy = [
-//   {
-//     id: "sdfnmsdklfnmsdlf",
-//     title: "Ride 1",
-//     date: "some date",
-//     pickup: "dfsd",
-//     dropoff: "sdfsdfsd",
-//     car_capacity: 4,
-//     price: 35,
-//     preferences: "sdfsdnfksdjf",
-//     creator: "sdfsd",
-//   },
-//   {
-//     id: "sdfgsdg",
-//     title: "Ride 2",
-//     date: "some date",
-//     pickup: "sfgdfg",
-//     dropoff: "sdfgsdrfh",
-//     car_capacity: 4,
-//     price: 35,
-//     preferences: "sdfgsdtgnfksdjf",
-//     creator: "Sdfsdf",
-//   },
-// ];
-
-// function Rides() {
-//   return (
-//     <section>
-//       <h1>Available Rides</h1>
-//       <RideList rides={dummy} />
-//     </section>
-//   );
-// }
 
 function Rides() {
   const [isLoading, setIsLoading] = useState(true);
