9b02bd74df1c4205172674abdd999b2789289db3
Roshan Shetty <roshanshetty2000@gmail.com>
2021-11-24 11:02:21 -0500

    minor changes

--- frontend/src/pages/JoinedRides.js
+++ frontend/src/pages/JoinedRides.js
@@ -1,22 +1,18 @@
 import React from "react";
+const axios = require("axios");
 
 // TODO: Frontend list, getting user and ride to be implemented
 
 function JoinedRides() {
-
   function joinRideHandler(ride) {
-    axios
-      .post("http://localhost:5001/joinRide", { ride })
-      .catch((err) => {
-        console.log(err.message);
+    axios.post("http://localhost:5001/joinRide", { ride }).catch((err) => {
+      console.log(err.message);
     });
   }
 
   function cancelJoinRideHandler(ride) {
-    axios
-      .post("http://localhost:5007/cancelJoin", { ride })
-      .catch((err) => {
-        console.log(err.message);
+    axios.post("http://localhost:5007/cancelJoin", { ride }).catch((err) => {
+      console.log(err.message);
     });
   }
 
--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 //require every js file that runs a microservice to start all microservices at once
-const eventBus = require("./eventbus")
-const auth = require("../routes/auth/auth")
-const create = require("../routes/createRide/createRide")
-const get = require("../routes/getRide/getRide")
-const cancel = require("../routes/cancelJoin/cancelJoin")
-const del = require("../routes/deleteRide/deleteRide")
-const join = require("../routes/joinRide/joinRide")
-const db = require("../routes/db/db")
+const eventBus = require("./eventbus");
+const auth = require("../routes/auth/auth");
+const create = require("../routes/createRide/createRide");
+const get = require("../routes/getRide/getRide");
+const cancel = require("../routes/cancelJoin/cancelJoin");
+const del = require("../routes/deleteRide/deleteRide");
+const join = require("../routes/joinRide/joinRide");
+const db = require("../routes/db/db");
