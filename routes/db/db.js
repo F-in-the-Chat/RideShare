2bbeb4b3f76323aeafbf06e80d1ee30cb375e50c
Rishab Maheshwari <40054912+rishabmahesh@users.noreply.github.com>
2021-11-29 22:29:51 -0500

    Generate token fix

--- routes/db/db.js
+++ routes/db/db.js
@@ -150,7 +150,7 @@ async function logging(event){
     throw new Error("Password doesn't match");
   }
 
-  let coin = info.methods.generateToken(); // check here
+  let coin = await info.generateToken();
   // info.token.push(coin); // check here
   // info.save();
 }
