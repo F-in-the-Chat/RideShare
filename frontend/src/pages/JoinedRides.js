de6447f8e580af57cb6cf91da0a076c579d54bce
Rishab Maheshwari <40054912+rishabmahesh@users.noreply.github.com>
2021-11-29 23:30:20 -0500

    Update JoinedRides.js
    Frontend implemented, have to implement backend

--- frontend/src/pages/JoinedRides.js
+++ frontend/src/pages/JoinedRides.js
@@ -1,22 +1,59 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import RideList from "../component/rides/RideList";
 const axios = require("axios");
 
 // TODO: Frontend list, getting user and ride to be implemented
 
-function JoinedRides() {
-  function joinRideHandler(ride) {
-    axios.post("http://localhost:5001/joinRide", { ride }).catch((err) => {
-      console.log(err.message);
-    });
-  }
-
-  function cancelJoinRideHandler(ride) {
+function cancelJoinRideHandler(ride) {
     axios.post("http://localhost:5007/cancelJoin", { ride }).catch((err) => {
-      console.log(err.message);
+        console.log(err.message);
     });
-  }
+}
 
-  return <div>Joined Rides Page</div>;
+function JoinedRides() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [joinedRides, setLoadedRides] = useState([]);
+    useEffect(() => {
+      setIsLoading(true);
+      fetch("http://localhost:5005/getRide")
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          const rides = [];
+          console.log(data);
+          for (const key in data) {
+            const ride = {
+              id: key,
+              ...data[key],
+            };
+
+            rides.push(ride);
+            console.log(rides[0]);
+          }
+          setIsLoading(false);
+          setLoadedRides(rides);
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+    }, []);
+
+    if (isLoading) {
+      return (
+        <section>
+          <p>Loading...</p>
+        </section>
+      );
+    }
+
+  return (
+    <section>
+      <h1>All the rides you have currently joined: -</h1>
+      <RideList rides={joinedRides} button = {"Leave Ride"} submitHandler2 = {cancelJoinRideHandler} />
+    </section>
+  )
 }
 
 export default JoinedRides;
