5e098896394277fde9fd387a5ed2df6c5b08af5d
Rishab Maheshwari <40054912+rishabmahesh@users.noreply.github.com>
2021-11-29 23:30:56 -0500

    Update JoinedRides.js
    Frontend implemented, have to implement backend

--- frontend/src/pages/JoinedRides.js
+++ frontend/src/pages/JoinedRides.js
@@ -1,59 +1,55 @@
-import React from "react";
 import { useState, useEffect } from "react";
 import RideList from "../component/rides/RideList";
 const axios = require("axios");
 
-// TODO: Frontend list, getting user and ride to be implemented
-
-function cancelJoinRideHandler(ride) {
-    axios.post("http://localhost:5007/cancelJoin", { ride }).catch((err) => {
+function joinRideHandler(ride) {
+    axios.post("http://localhost:5001/joinRide", { ride }).catch((err) => {
         console.log(err.message);
     });
 }
 
-function JoinedRides() {
-    const [isLoading, setIsLoading] = useState(true);
-    const [joinedRides, setLoadedRides] = useState([]);
-    useEffect(() => {
-      setIsLoading(true);
-      fetch("http://localhost:5005/getRide")
-        .then((res) => {
-          return res.json();
-        })
-        .then((data) => {
-          const rides = [];
-          console.log(data);
-          for (const key in data) {
-            const ride = {
-              id: key,
-              ...data[key],
-            };
+function Rides() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedRides, setLoadedRides] = useState([]);
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("http://localhost:5005/getRide")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        const rides = [];
+        console.log(data);
+        for (const key in data) {
+          const ride = {
+            id: key,
+            ...data[key],
+          };
 
-            rides.push(ride);
-            console.log(rides[0]);
-          }
-          setIsLoading(false);
-          setLoadedRides(rides);
-        })
-        .catch((err) => {
-          console.log(err.message);
-        });
-    }, []);
+          rides.push(ride);
+          console.log(rides[0]);
+        }
+        setIsLoading(false);
+        setLoadedRides(rides);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
 
-    if (isLoading) {
-      return (
-        <section>
-          <p>Loading...</p>
-        </section>
-      );
-    }
-
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
   return (
     <section>
-      <h1>All the rides you have currently joined: -</h1>
-      <RideList rides={joinedRides} button = {"Leave Ride"} submitHandler2 = {cancelJoinRideHandler} />
+      <h1>Available Rides</h1>
+      <RideList rides={loadedRides} button={"Join Ride"} submitHandler={joinRideHandler}/>
     </section>
-  )
+  );
 }
 
-export default JoinedRides;
+export default Rides;
