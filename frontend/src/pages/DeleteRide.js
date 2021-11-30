0ad4cc7e321b6128392011c4a75612b1e597c2dc
Rishab Maheshwari <40054912+rishabmahesh@users.noreply.github.com>
2021-11-29 23:29:48 -0500

    Update DeleteRide.js
    Frontend implemented, have to implement backend

--- frontend/src/pages/DeleteRide.js
+++ frontend/src/pages/DeleteRide.js
@@ -1,12 +1,23 @@
 import { useState, useEffect } from "react";
-// import SecondRideList from "../component/rides/SecondRideList";
+import RideList from "../component/rides/RideList";
 const axios = require("axios");
 
+
+function deleteRideHandler(ride) {
+  // deleting ride using id only
+  axios
+    .post("http://localhost:5006/deleteRide", { ride })
+    .catch((err) => {
+      console.log(err.message);
+    });
+}
+
+
 // HAVE TO EDIT THIS SUCH THAT WE GET THE RIDES MADE BY THE PARTICULAR USER - JUST DISPLAYING ALL RIDES FOR NOW
-// MIGHT HAVE TO LINK RIDES WITH USERS (ESPECIALLY DRIVERS)
+// MIGHT HAVE TO LINK RIDES WITH USERS (ESPECIALLY DRIVERS) 
 function DeleteRide() {
   const [isLoading, setIsLoading] = useState(true);
-  const [loadedRides, setLoadedRides] = useState([]);
+  const [driverRides, setLoadedRides] = useState([]);
   useEffect(() => {
     setIsLoading(true);
     fetch("http://localhost:5005/getRide")
@@ -39,18 +50,11 @@ function DeleteRide() {
     );
   }
 
-  function deleteRideHandler(ride) {
-    // deleting ride using title name only
-    axios.post("http://localhost:5006/deleteRide", { ride }).catch((err) => {
-      console.log(err.message);
-    });
-  }
-
   return (
-    <section>
-      <h1>Rides Created by You</h1>
-      {/* <SecondRideList rides={loadedRides} deleteRide={deleteRideHandler} /> */}
-    </section>
+      <section>
+          <h1>Rides Created by You</h1>
+          <RideList rides={driverRides} deleteRide={deleteRideHandler} button={"Delete Ride"} />
+      </section>
   );
 }
 
