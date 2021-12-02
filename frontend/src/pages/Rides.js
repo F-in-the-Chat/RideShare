0c87864c80ca2ec020fcb04ed3d5008594e2082f
Roshan Shetty <roshanshetty2000@gmail.com>
2021-12-02 16:02:37 -0500

    code cleanup

--- frontend/src/pages/Rides.js
+++ frontend/src/pages/Rides.js
@@ -3,24 +3,29 @@ import TokenContext from "../AppContext";
 import RideList from "../component/rides/RideList";
 const axios = require("axios");
 
-function joinRideHandler(ride,token) {
-    console.log("inside joinRideHandler")
-    axios.post("http://localhost:5001/joinRide", { ride:ride.id,user:token }).catch((err) => {
-        console.log(err.message);
+function joinRideHandler(ride, token) {
+  console.log("inside joinRideHandler");
+  axios
+    .post("http://localhost:5001/joinRide", { ride: ride.id, user: token })
+    .catch((err) => {
+      console.log(err.message);
     });
 }
 
-function cancelRideHandler(ride,token) {
-  console.log(ride)
-  axios.post(`http://localhost:5007/cancelJoin`, { ride:ride.id,user:token }).catch((err) => {
+function cancelRideHandler(ride, token) {
+  console.log(ride);
+  axios
+    .post(`http://localhost:5007/cancelJoin`, { ride: ride.id, user: token })
+    .catch((err) => {
       console.log(err.message);
-  });
+    });
 }
 
 function Rides() {
   const [isLoading, setIsLoading] = useState(true);
   const [loadedRides, setLoadedRides] = useState([]);
-  const {tokenContext,setToken} = useContext(TokenContext)
+  const { tokenContext, setToken } = useContext(TokenContext);
+  console.log(tokenContext);
   useEffect(() => {
     setIsLoading(true);
     fetch("http://localhost:5005/getRide")
@@ -54,12 +59,22 @@ function Rides() {
   }
   return (
     <section>
-      {tokenContext!="null"?<div>
-        <h1>My Rides:</h1>
-        <RideList rides={[]} button={"Cancel Ride"} submitHandler={cancelRideHandler}/>
-      </div>:null}
+      {tokenContext !== "null" ? (
+        <div>
+          <h1>My Rides:</h1>
+          <RideList
+            rides={[]}
+            button={"Cancel Ride"}
+            submitHandler={cancelRideHandler}
+          />
+        </div>
+      ) : null}
       <h1>Available Rides: -</h1>
-      <RideList rides={loadedRides} button={"Join Ride"} submitHandler={joinRideHandler}/>
+      <RideList
+        rides={loadedRides}
+        button={"Join Ride"}
+        submitHandler={joinRideHandler}
+      />
     </section>
   );
 }
